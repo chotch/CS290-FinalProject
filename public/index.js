0cda85d088fde01564b69bb85500523c5787413c
byko15 <38079674+byko15@users.noreply.github.com>
2018-12-03 12:42:36 -0800

    Updated index.js for modal function
    Almost finished with it, touch ups needed

--- public/index.js
+++ public/index.js
@@ -1,3 +1,26 @@
+var addModalopen = document.getElementById('add-exercise-button');
+var modal = document.getElementById('add-modal');
+var modalBackdrop = document.getElementById('modal-add-backdrop');
+
+function addExercise()
+{
+  modal.classList.remove('hidden');
+  modalBackdrop.classList.removed('hidden');
+    console.log('== modal opened');
+}
+
+function modalCancel()
+{
+  modal.classList.add('hidden');
+  modalBackdrop.classList.add('hidden');
+    console.log('== modal closed');
+}
+
+function modalAccept()
+{
+  
+}
+
 var filterFindButton = document.getElementById('filter-find-button');
 if (filterFindButton){
   filterFindButton.addEventListener('click', updateExercises);
@@ -22,10 +45,17 @@ if (addExerciseButton){
   addExerciseButton.addEventListener('click', addExercise);
 }
 
-
-function addExercise(){
-  //open up a new window. User enters name of exercise, reps, sets, and day
+var modalCancel = document.getElementById('modal-cancel');
+if(modalCancel)
+{
+  modalCancel.addEventListener('click', modalCancel);
 }
+var modalAccept = document.getElementById('modal-accept');
+if(modalAccept)
+{
+  modalAccept.addEventListener('click', modalAccept);
+}
+
 
 
 var viewPlanButton = document.getElementById('viewPlan-button');
