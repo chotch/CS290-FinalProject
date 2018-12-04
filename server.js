5af8300fadc39824563c2449cdf75e51747f0580
Justin Mai <justintmai@gmail.com>
2018-12-03 18:48:53 -0800

    re-formatting server.js
    formatting server.js auto-indent
    
    re-formatting server.js

--- server.js
+++ server.js
@@ -10,29 +10,35 @@ var exerciseData = require('./exerciseData');
 
 // register `express-handlebars` with Express as a template engine
 //  main' is derived from the name of our layout template file, `views/layouts/main.handlebars`
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // tell Express to use `express-handlebars` by default every time
 app.set('view engine', 'handlebars');
 
+// start listening for requests on port 3000
+app.listen(port, function () {
+  console.log('server is listening on port', port);
+});
 
+
+/* 
+        ROUTING
+*/
 
 // serve the homepage (ie. homePage.handlebars)
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
   res.render('homePage', {
     // pass in the context object containing our exercise data
     exercises: exerciseData
   });
-app.get('/favicon.ico', function(req, res)
-{
+});
+
+// silence "couldn't find favicon.ico" error
+app.get('/favicon.ico', function (req, res) {
   app.status(200);
   app.end();
 });
-  console.log('rendering view template');
-});
 
-// Serve any files matching those in the "./public" directory
+// serve any files matching those in the "./public" directory
 app.use(express.static('public'));
 
-app.listen(port, function (){
-  console.log('server is listening on port', port);
-});
+
